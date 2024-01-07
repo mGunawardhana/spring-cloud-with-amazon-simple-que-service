59e03e24b272de203b2e86161f09701c30e50980
mGunawardhana <mrgunawardhana27368@gmail.com>
2024-01-07 07:53:11 +0530

    introducing

---
+++ src/main/java/com/mgunawardhana/aws/sqs/SpringbootAwsSqsExeApplication.java
@@ -0,0 +1,13 @@
+package com.mgunawardhana.aws.sqs;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringbootAwsSqsExeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringbootAwsSqsExeApplication.class, args);
+	}
+
+}
